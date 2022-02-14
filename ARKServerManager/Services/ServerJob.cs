3084e4ddd030cd72520e271a079f5b7bbb321dc9
Александр Паршуков <20384838+LeOn0408@users.noreply.github.com>
2022-02-14 09:39:36 +0300

    fix BackupJobs

--- ARKServerManager/Services/ServerJob.cs
+++ ARKServerManager/Services/ServerJob.cs
@@ -14,55 +14,57 @@ namespace ARKServerManager.ServerService
     public class ServerJob:IHostedService
     {
         private List<int> process=new();
-        public IConfiguration AppConfiguration { get; private set; }
         private readonly IServiceScopeFactory scopeFactory;
         private readonly ILogger<ServerJob> logger;
         
         private Dictionary<int, ServerTask> ActiveTask { get; set; }
         List<ServerTask> Tasks { get; set; }
 
+        private Timer timer;
+
         public ServerJob(IServiceScopeFactory _scopeFactory, ILogger<ServerJob> _logger)
         {
             scopeFactory = _scopeFactory;
             logger = _logger;
-            
+            ActiveTask = new Dictionary<int, ServerTask>();
         }
         public Task StartAsync(CancellationToken cancellationToken)
         {
             string message = $"{DateTime.Now:yyyy.MM.dd HH:mm:ss} Background tasks started";
             logger.LogInformation(message);
-            Timer timer = new(GetJobs, null, 0, 60000);
-            ActiveTask = new Dictionary<int, ServerTask>();
+            timer = new(GetJobs, null, 0, 60000);
+            
             return Task.CompletedTask;
         }
         public void GetJobs(object state)
         {
             using IServiceScope scope = scopeFactory.CreateScope();
             using var Db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-            
             try
             {
-                
+                Statistics statistics = new(scopeFactory);
+                statistics.StartStatistics();
+
                 Tasks = Db.Jobs.Where(x => x.Result == 0).AsNoTracking().ToList();
                 foreach (ServerTask c in Tasks)
                 {
                     bool activeTask = ActiveTask.ContainsKey(c.Id);
                     //вычислить разницу
                     TimeSpan timeToJob = c.DateJob.Subtract(DateTime.Now);
-                    if (timeToJob.Hours <= 0 && timeToJob.Minutes <= 5 && !activeTask)
+                    if (timeToJob.Hours <= 0 && timeToJob.Minutes <= 1 && !activeTask)
                     {
                         ActiveTask.Add(c.Id, c);
                         Task task = new(() => JobsCreated(c));
                         task.Start();
                     }
                 }
+                
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
+                logger.LogInformation(ex.Message);
             }
-            Statistics statistics = new(scopeFactory);
-            statistics.StartStatistics();
+            
         }
         void JobsCreated(ServerTask c)
         {
@@ -75,20 +77,21 @@ namespace ARKServerManager.ServerService
                 Thread.Sleep(timeToJob);
             }
             
-            ServerTask edit = Db.Jobs.FirstOrDefault(x => x.Id == c.Id);
+            //ServerTask edit = Db.Jobs.FirstOrDefault(x => x.Id == c.Id);
             if (c.Repeating)
             {
-                int day = (int)DateTime.Now.Subtract(c.DateJob).TotalDays;
-                edit.DateJob = c.DateJob.AddDays(day);
+                int day = (int)DateTime.Now.Subtract(c.DateJob).TotalDays + 1;
+                c.DateJob = c.DateJob.AddDays(day);
             }
+            Db.Jobs.Update(c);
             Db.SaveChanges();
-            
+            ActiveTask.Remove(c.Id);
             try
             {
                 switch (c.Type)
                 {
                     case 1: 
-                        Backup(Db, edit);
+                        Backup(Db, c);
                         break;
                     case 2:
                         //TODO: Доработать
@@ -101,7 +104,7 @@ namespace ARKServerManager.ServerService
             {
                 logger.LogError($"{DateTime.Now:yyyy.MM.dd HH:mm:ss} {ex.Message}");
             }
-            ActiveTask.Remove(c.Id);
+            
 
         }
         private async void Update(DatabaseContext Db, ServerTask edit)
@@ -111,7 +114,7 @@ namespace ARKServerManager.ServerService
             ArkServer ark = new();
 
             await ark.Save(server);
-            StopChildrenProcess();//ркон не всегда корректно срабатывает. Принудительно закрываем все ранее запущенные приложения, хотя лучше использовать коммнду quit
+            //StopChildrenProcess();//ркон не всегда корректно срабатывает. Принудительно закрываем все ранее запущенные приложения, хотя лучше использовать коммнду quit
             
             await ark.UpdateAsync(logger);
             process.Add(ark.Launch(server));
@@ -127,20 +130,21 @@ namespace ARKServerManager.ServerService
             
         }
 
-        void StopChildrenProcess()
-        {
-            foreach (var item in process)
-            {
-                var proc = Process.GetProcessById(item);
-                proc.Kill(true);
-                process.Remove(item);
-            }
+        //void StopChildrenProcess()
+        //{
+        //    foreach (var item in process)
+        //    {
+        //        var proc = Process.GetProcessById(item);
+        //        proc.Kill(true);
+        //        process.Remove(item);
+        //    }
 
-            //TODO: Добавить очистку всех серверов, которые не контролируются нами. Или выдать предупреждение что обнаружены не закрытые сервера
-        }
+        //    //TODO: Добавить очистку всех серверов, которые не контролируются нами. Или выдать предупреждение что обнаружены не закрытые сервера
+        //}
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            StopChildrenProcess();
+            //StopChildrenProcess();
+            timer.Dispose();
             return Task.CompletedTask;
         }
     }
