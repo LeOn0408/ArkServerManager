944d6395266b28ab1a2280619e08517710f4da95
Александр Паршуков <20384838+LeOn0408@users.noreply.github.com>
2022-07-13 15:31:37 +0300

    parser log

---
+++ ARKServerManager/Models/Ark/ArkLogRow.cs
@@ -0,0 +1,10 @@
+﻿namespace ARKServerManager.Models.Ark
+{
+    public class ArkLogRow
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public int RecordID { get; set; }
+        public string RecordRow { get; set; }
+    }
+}
